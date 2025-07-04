d33e3f745625dbd9c813f0105e2a3d76c6075d4c
Jorgecs14 <146192129+Jorgecs14@users.noreply.github.com>
2025-07-04 17:43:32 +0200

    feat/vercelJson

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
-
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
@@ -13,18 +12,22 @@ const ecommerceRouter = require('./routes/ecommerce')
 const foodsRouter = require('./routes/foods')
 const dietFoodsRouter = require('./routes/diet_foods')
 
-
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use((req, res, next) => {
   console.log(`Petición recibida: ${req.method} ${req.url}`)
   next()
 })
-
 
 // Rutas públicas (no requieren autenticación)
 app.use('/api/users', usersRouter)
@@ -35,7 +38,6 @@ app.use('/api/exercises', authenticateToken, exercisesRouter)
 app.use('/api/workouts_exercises', authenticateToken, workoutsExercisesRouter)
 app.use('/api/foods', authenticateToken, foodsRouter)
 app.use('/api/diet_foods', authenticateToken, dietFoodsRouter)
-
 
 app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`)
@@ -49,4 +51,3 @@ app.listen(PORT, () => {
   console.log('  - /api/foods')
   console.log('  - /api/diet_foods')
 })
-
